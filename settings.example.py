3aa130902982836b5adc5c1399cd32916907b47d
2gn <101851090+2gn@users.noreply.github.com>
2025-03-05 18:49:11 +0900

    added example settings

---
+++ settings.example.py
@@ -0,0 +1,5 @@
+# rename to settings.py
+DOMAIN="your-doma.in"
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/12345678901234/alsdHfh0brBSSosxckz2IhfEjzOVoucxhjjlkajsV0uoaDsHLfasdjHfeuvnwldJvAhd"
+
